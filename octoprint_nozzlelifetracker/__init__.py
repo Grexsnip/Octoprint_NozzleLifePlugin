af3ad6d084433d991d2bb08eca7cd2c8e239d096
Grexsnip <andy.rabin@gmail.com>
2025-08-25 15:12:00 -0500

    Update __init__.py
    added get_assets() function
    increased rev to 0.2.7

--- octoprint_nozzlelifetracker/__init__.py
+++ octoprint_nozzlelifetracker/__init__.py
@@ -15,7 +15,7 @@ from flask import make_response, request
 import csv
 
 __plugin_name__ = "Nozzle Life Tracker"
-__plugin_version__ = "0.2.6"
+__plugin_version__ = "0.2.7"
 __plugin_pythoncompat__ = ">=3.7,<3.12"
 __plugin_octoprint_version__ = ">=1.9,<2"
 
@@ -38,6 +38,15 @@ class NozzleLifeTrackerPlugin(StartupPlugin,
     def on_after_startup(self):
         self._logger.info("NozzleLifeTracker plugin started.")
         self._load_nozzles()
+
+    ##~~ Assets
+
+    def get_assets(self):
+        return {
+            "js": ["js/nozzlelifetracker.js"],
+            "css": ["css/nozzlelifetracker.css"],
+            "less": ["less/nozzlelifetracker.less"],
+        }
 
     ##~~ SettingsPlugin
 
